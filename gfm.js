011f6a9cc6800cf39ec36b7e7d9cb52c6ff8c997
Linz Jax <linzjax@gmail.com>
2015-09-02 17:48:23 -0400

    api keys removed (take that) and code cleaned

--- gfm.js
+++ gfm.js
@@ -1,22 +1,43 @@
-//dark skies api key : dfca3c0aef158e73d2cc5951a910b3d6
-//https://api.forecast.io/forecast/dfca3c0aef158e73d2cc5951a910b3d6/LATITUDE,LONGITUDE
+//Determine the time of day and the date. Will update every second.
+function updateClock(){
+	var date = new Date().getHours();
+	var minutes = ('0' + new Date().getMinutes()).slice(-2);
+	var x = new Date().getMonth();
+	var month = month_name[x];
+	var day = new Date().getDate();
+	var year = new Date().getFullYear();
 
-//google geolocator api key:
-var api_key = "AIzaSyCwz9UmMiTedyaMLToME_GZfAO-KQcFico";
+	function getHour() {
+		var hour;
+		if (date > 12){
+			return date - 12 + ":" + minutes +  'pm';
+		} else {
+			return date + ":" + minutes + 'am';
+		}
+	}
 
-var date = new Date().getHours();
+	var todays_date = getHour() + '<br>' + month + ' ' + day + ", " + year;
+	document.getElementById('current_time'). innerHTML = todays_date;
+	checkTime(date);
+	setTimeout(updateClock, 1000);
+}//end updateClock
 
-
+//determines whether to display morning or afternoon
+function checkTime(date){
+	var timeOfDay = document.getElementById('timeOfDay');
+	if (date < 11)
+		timeOfDay.innerHTML = "Good freakin' morning!";
+	else
+		timeOfDay.innerHTML = "Good freakin' afternoon!";
+}//end checkTime
 
 //generate the compliment
 //compliments are stored in compliments.js
 function giveCompliment(){
-	var x = Math.floor(Math.random() * compliments.length + 1);
+	var x = Math.floor(Math.random() * compliments.length);
 	var chosenCompliment = compliments[x].compliment;
 	document.getElementById('complimentMe').innerHTML = chosenCompliment;
 }//end giveCompliment
-
-
 
 
 //generate the background
@@ -26,61 +47,55 @@ function changeBackground(){
 	document.body.setAttribute("style", "background-image: url('img/" + img_url[x] + "');");
 }//end change background
 
-
-
-
-//determines whether to display morning or afternoon
-function checkTime(){
-	var timeOfDay = document.getElementById('timeOfDay');
-	if (date < 11)
-		timeOfDay.innerHTML = "Good freakin' morning!";
-	else
-		timeOfDay.innerHTML = "Good freakin' afternoon!";
-}//end checkTime
-
-
-
-
-
-
-
-//
+//get location of user and trigger a data stroage
 var x = document.getElementById("weather");
 function displayWeather() {
 	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(accessDarkSkies);
+		navigator.geolocation.getCurrentPosition(storeData);
 	} else {
 		x.innerHTML = "well, guess I can't creep";
 	}
 }
-
-function showPosition(position){
-	x.innerHTML = "latitude: " + position.coords.latitude + "<br>Longitute: " + position.coords.longitute;
+//store the users long and lat for faster weather generation
+function storeData(position){
+	var lat = position.coords.latitude;
+	var lgn = position.coords.longitude;
+	chrome.storage.sync.set({
+		"latitude" : lat,
+		"longitude" : lgn
+	}, function(){
+		console.log('things ' + lat + " " + lgn + ' were saved');
+	});
 }
 
-
-
+//display the approprate weather information based on users location and Dark Sky's api
 function accessDarkSkies(position){
-	var uri = "https://api.forecast.io/forecast/dfca3c0aef158e73d2cc5951a910b3d6/"+ position.coords.latitude + "," + position.coords.longitude;
-	var xhr = new XMLHttpRequest();
-	xhr.onreadystatechange = function(){
-			if (xhr.readyState === 4 && xhr.status === 200){
-			var j = JSON.parse(xhr.responseText);
-			x.innerHTML = j.currently.apparentTemperature;
+	chrome.storage.sync.get(function(data){
+		if (!data){
+			displayWeather();
 		}
-	};
+		var uri = "https://api.forecast.io/forecast/" + darkskieskey + "/"+ data.latitude + "," + data.longitude;
+		var xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function(){
+				if (xhr.readyState === 4 && xhr.status === 200){
+				var j = JSON.parse(xhr.responseText);
+				var temp = j.currently.temperature.toString();
+				temp = temp.split('.');
+				x.innerHTML = temp[0] + '&#8457 <br>' + j.currently.summary;
+			}
+		};
 
-	xhr.open("GET", uri);
-	xhr.send();
-}
+		xhr.open("GET", uri);
+		xhr.send();
+		//end xhr
+	});//end storage get
+}//end access dark skies.
 
-
-
-
-
+//kick everything off once them DOM has loaded
 document.addEventListener('DOMContentLoaded', function() {
+	updateClock();
 	changeBackground();
 	checkTime();
 	giveCompliment();
-	displayWeather();
+	accessDarkSkies();
 });
